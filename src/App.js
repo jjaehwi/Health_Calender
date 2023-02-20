ed1ab8ab7ef12bb117a519acfd0ff9d6fbf8c233
HWI <dlwognl99@naver.com>
2023-02-20 15:47:40 +0900

    init: 프로젝트 생성

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import MyCalender from "./MyCalender";
+
+function App() {
+  return (
+    <div className="App">
+      <MyCalender />
+    </div>
+  );
+}
+
+export default App;
